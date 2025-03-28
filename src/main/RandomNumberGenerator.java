88f71ffb47214ba81dd8de82d54894c613fd9346
michalj <mjanuszkiewicz2004@gmail.com>
2025-03-28 15:22:37 +0100

    translation to english

--- src/main/RandomNumberGenerator.java
+++ src/main/RandomNumberGenerator.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public abstract class RandomNumberGenerator {
     private static final Random generator = new Random();
 
+    //  generates a random number between begin and end, inclusive
     public static int randInt(int begin, int end) {
         return generator.nextInt(begin, end + 1);
     }
